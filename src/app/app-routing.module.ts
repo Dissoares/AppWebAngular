c310d6eb848bb7ea476c89fb9e68ec751d47e33f
Dissoares <Diegosoaresyt@gmail.com>
2023-07-12 00:01:05 -0300

    Criando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ContentComponent } from './layout/content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContentComponent },
+  { path: 'Header', component: HeaderComponent },
+  { path: 'SidebAr', component: SidebarComponent },
+  { path: 'Footer', component: FooterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
